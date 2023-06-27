5d3ba20fd0b94a1d29910571deac2a6f83296ad3
sangjae84 <135927747+sangjae84@users.noreply.github.com>
2023-06-27 15:26:55 +0900

    Update 07_Distribution of TV show season counts in South Korea and the United States.py

--- pages/07_Distribution of TV show season counts in South Korea and the United States.py
+++ pages/07_Distribution of TV show season counts in South Korea and the United States.py
@@ -2,34 +2,45 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import common
+
 common.page_config()
-st.title("Number of productions by year in South Korea and the United States")
+
+st.title("Distribution of genres in South Korea and the United States")
+
 data = common.get_sales()
+
 # Tab 구성
 tab1, tab2 = st.tabs(["South Korea", "United States"])
+
 sk_data = data[data['country'] == 'South Korea']
 usa_data = data[data['country'] == 'United States']
-sk_type_counts = sk_data['release_year'].value_counts().sort_index()
-type_counts = usa_data['release_year'].value_counts().sort_index()
+
+data['main_genre'] = data['listed_in'].str.split(',').str[0]
+
+genre_counts = sk_data['listed_in'].str.split(',').explode().str.strip().value_counts()
+genre_counts = data['listed_in'].str.split(',').explode().str.strip().value_counts()
+
+genre_table = pd.DataFrame({'Genre': genre_counts.index, 'Count': genre_counts})
+top_10_genres = genre_table.head(10)
+
 with tab1:
-    #색깔
-  color = ['violet']
-  #선그래프
-  plt.bar(sk_type_counts.index, sk_type_counts.values, color= color)
-  plt.xlabel('Year')
-  plt.ylabel('Count')
-  plt.xticks(sk_type_counts.index, sk_type_counts.index.astype(int), rotation=45)
+  #파이 차트
+  plt.pie(top_10_genres.Count, labels=top_10_genres.index, autopct='%1.1f%%')
+    
+  #차트 제목
   plt.title('Netflix Shows in the South Korea')
+    
+  #출력
   plt.show()
   st.pyplot(plt)
+
 with tab2:
-    #색깔
-  color = ['olive']
-  #선그래프
-  plt.bar(type_counts.index, type_counts.values, color= color)
-  plt.xlabel('Year')
-  plt.ylabel('Count')
-  plt.xticks(rotation=45)
+  #파이 차트
+  plt.pie(top_10_genres.Count, labels=top_10_genres.index, autopct='%1.1f%%')
+
+  #차트 제목
   plt.title('Netflix Shows in the United States')
+
+  #출력
   plt.show()
   st.pyplot(plt)
