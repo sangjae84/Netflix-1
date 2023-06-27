384aa1223aaa29de93e660bfc7eca3fbfc663e57
sangjae84 <135927747+sangjae84@users.noreply.github.com>
2023-06-27 15:44:22 +0900

    Update 06_Distribution of movie durations in South Korea and the United States.py

--- pages/06_Distribution of movie durations in South Korea and the United States.py
+++ pages/06_Distribution of movie durations in South Korea and the United States.py
@@ -7,3 +7,7 @@ common.page_config()
 
 st.title("Distribution of movie durations in South Korea and the United States")
 
+data = common.get_sales()
+
+sk_data = data[data['country'] == 'South Korea']
+usa_data = data[data['country'] == 'United States']
