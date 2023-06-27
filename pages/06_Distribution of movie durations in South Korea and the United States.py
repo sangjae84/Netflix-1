63acec1059d8c2e53633652eafd3faf3fcbb0cf9
sangjae84 <135927747+sangjae84@users.noreply.github.com>
2023-06-27 15:48:57 +0900

    Update 06_Distribution of movie durations in South Korea and the United States.py

--- pages/06_Distribution of movie durations in South Korea and the United States.py
+++ pages/06_Distribution of movie durations in South Korea and the United States.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import pandas as pd
 import plotly.graph_objects as go
 import common
+import seaborn as sns
 
 common.page_config()
 
@@ -9,5 +11,27 @@ st.title("Distribution of movie durations in South Korea and the United States")
 
 data = common.get_sales()
 
-sk_data = data[data['country'] == 'South Korea']
-usa_data = data[data['country'] == 'United States']
+
+
+# 데이터 필터링
+sk_movies_data = data[(data['country'] == 'South Korea') & (data['type'] == 'Movie')]
+duration_sk = sk_movies_data['duration'].str.replace(' min', '').astype(int)
+
+usa_movies_data = data[(data['country'] == 'United States') & (data['type'] == 'Movie')]
+duration_usa = usa_movies_data['duration'].str.replace(' min', '').astype(int)
+
+# 그래프 설정
+fig, ax = plt.subplots(figsize=(10, 6))
+plt.hist(duration_sk, bins=30, density=True, alpha=0.5, color='red', label='South Korea')
+sns.kdeplot(duration_sk, color='red', label='South Korea')
+plt.hist(duration_usa, bins=30, density=True, alpha=0.5, color='green', label='United States')
+sns.kdeplot(duration_usa, color='green', label='United States')
+plt.title('Distribution of Movie Durations for Netflix Content')
+plt.xlabel('Duration (minutes)')
+plt.ylabel('Density')
+plt.legend()
+
+# Streamlit 앱 설정
+st.set_page_config(layout="wide")
+st.title("Distribution of movie durations in South Korea and the United States")
+st.pyplot(fig)
