7912b78eb34b22867cdd6e1d1f65fdfc46e7abc6
sangjae84 <135927747+sangjae84@users.noreply.github.com>
2023-06-27 15:49:46 +0900

    Update 06_Distribution of movie durations in South Korea and the United States.py

--- pages/06_Distribution of movie durations in South Korea and the United States.py
+++ pages/06_Distribution of movie durations in South Korea and the United States.py
@@ -13,13 +13,6 @@ data = common.get_sales()
 
 
 
-# 데이터 필터링
-sk_movies_data = data[(data['country'] == 'South Korea') & (data['type'] == 'Movie')]
-duration_sk = sk_movies_data['duration'].str.replace(' min', '').astype(int)
-
-usa_movies_data = data[(data['country'] == 'United States') & (data['type'] == 'Movie')]
-duration_usa = usa_movies_data['duration'].str.replace(' min', '').astype(int)
-
 # 그래프 설정
 fig, ax = plt.subplots(figsize=(10, 6))
 plt.hist(duration_sk, bins=30, density=True, alpha=0.5, color='red', label='South Korea')
