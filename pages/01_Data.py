739cddfea92c14079d92aef618ad8959cc68f139
Minjung Kang <135298407+minjung00@users.noreply.github.com>
2023-06-27 12:34:44 +0900

    Update 01_Data.py

--- pages/01_Data.py
+++ pages/01_Data.py
@@ -6,9 +6,10 @@ import numpy as np
 import seaborn as sns
 
 st.title("South Korea-Data")
+data = pd.read_csv("./netflix1.csv")
 sk_data = data[data['country'] == 'South Korea']
 st.write(sk_data)
+
 st.title("US-Data")
-data = pd.read_csv("./netflix1.csv")
 usa_data = data[data['country'] == 'United States']
 st.write(usa_data)
