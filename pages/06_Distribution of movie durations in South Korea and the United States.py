003ee0f34a52dbaa9cfa94784a22ef24eb2cbc03
sangjae84 <135927747+sangjae84@users.noreply.github.com>
2023-06-27 15:53:48 +0900

    Update 06_Distribution of movie durations in South Korea and the United States.py

--- pages/06_Distribution of movie durations in South Korea and the United States.py
+++ pages/06_Distribution of movie durations in South Korea and the United States.py
@@ -11,20 +11,9 @@ st.title("Distribution of movie durations in South Korea and the United States")
 
 data = common.get_sales()
 
+# Tab 구성
+tab1, tab2, tab3 = st.tabs(["South Korea", "United States"," "])
 
+sk_data = data[data['country'] == 'South Korea']
+usa_data = data[data['country'] == 'United States']
 
-# 그래프 설정
-fig, ax = plt.subplots(figsize=(10, 6))
-plt.hist(duration_sk, bins=30, density=True, alpha=0.5, color='red', label='South Korea')
-sns.kdeplot(duration_sk, color='red', label='South Korea')
-plt.hist(duration_usa, bins=30, density=True, alpha=0.5, color='green', label='United States')
-sns.kdeplot(duration_usa, color='green', label='United States')
-plt.title('Distribution of Movie Durations for Netflix Content')
-plt.xlabel('Duration (minutes)')
-plt.ylabel('Density')
-plt.legend()
-
-# Streamlit 앱 설정
-st.set_page_config(layout="wide")
-st.title("Distribution of movie durations in South Korea and the United States")
-st.pyplot(fig)
